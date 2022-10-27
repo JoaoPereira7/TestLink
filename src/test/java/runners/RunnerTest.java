6717af99612d30a3cdbfde2af42399f4ea741309
JoaoPereira7 <77362779+JoaoPereira7@users.noreply.github.com>
2022-10-27 16:17:17 -0300

    Update RunnerTest.java

--- src/test/java/runners/RunnerTest.java
+++ src/test/java/runners/RunnerTest.java
@@ -8,7 +8,7 @@ import io.cucumber.junit.CucumberOptions.SnippetType;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		plugin = {"pretty"},
+		plugin = {"pretty", "html:target/cucumber-report"}},
 		features = "src/test/resources/features",
 		glue = "stepsDefinitions",
 //		tags = "",
